615dd54b8a1e9e01660d7b475fa773c31f570691
Junior <jtavarez8@gmail.com>
2019-05-12 01:18:44 -0400

    Updating Indecision APP

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,44 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running!');
+console.log("App.js is running!");
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
+  "div",
   null,
-  ' does this change'
+  React.createElement(
+    "h1",
+    null,
+    " Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  ";"
 );
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Junior Tavarez"
+  ),
+  React.createElement(
+    "p",
+    null,
+    " Age: 30"
+  ),
+  React.createElement(
+    "p",
+    null,
+    " Location: NorthCarolina"
+  )
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-console.log('App.js is running!')
+console.log("App.js is running!");
 
 // JSX - JavaScript XML
-var template = <h1> does this change</h1>
-  var appRoot = document.getElementById('app');
-  ReactDOM.render(template, appRoot);
+var template = (
+  <div>
+    <h1> Indecision App</h1>
+    <p>This is some info</p>;
+  </div>
+);
+
+var template2 = (
+  <div>
+    <h1>Junior Tavarez</h1>
+    <p> Age: 30</p>
+    <p> Location: NorthCarolina</p>
+  </div>
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template2, appRoot);
